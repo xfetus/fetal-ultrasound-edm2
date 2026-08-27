4de921db98117ff9c6d7600534fd580cefdd638f
Miguel Xochicale <m.xochicale@ucl.ac.uk>
2026-08-28 00:59:16 +0100

    solve issues for a clean PASSED pre-commit hooks

--- torch_utils/misc.py
+++ torch_utils/misc.py
@@ -273,7 +273,7 @@ def print_module_summary(module, inputs, max_nesting=3, skip_redundant=True):
     hooks += [mod.register_forward_hook(post_hook) for mod in module.modules()]
 
     # Run module.
-    outputs = module(*inputs)
+    outputs = module(*inputs)  # noqa: F841
     for hook in hooks:
         hook.remove()
 
